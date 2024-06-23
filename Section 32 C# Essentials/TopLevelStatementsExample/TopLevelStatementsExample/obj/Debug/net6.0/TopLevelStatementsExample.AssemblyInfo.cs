46d27fc6407a9eab23530c00d1d9908626a26f17
Bikash Dumrakoti <bikasdumrakoti@gmail.com>
2024-06-23 16:59:29 +0545

    Changes in internal application files.

--- Section 32 C# Essentials/TopLevelStatementsExample/TopLevelStatementsExample/obj/Debug/net6.0/TopLevelStatementsExample.AssemblyInfo.cs
+++ Section 32 C# Essentials/TopLevelStatementsExample/TopLevelStatementsExample/obj/Debug/net6.0/TopLevelStatementsExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopLevelStatementsExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8988ed76d69c888e77e865b027a579b0846351a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1e6ab7bdf83387345bf8a26ea3b3105bc4d8e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopLevelStatementsExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopLevelStatementsExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
